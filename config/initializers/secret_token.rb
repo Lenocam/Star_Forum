ac93f9002b20c23944c2b71280028b6ec7a1f872
Reed MacConnell <reed.macconnell@gmail.com>
2014-02-28 11:20:57 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StarForum::Application.config.secret_key_base = 'd067263aade916b3f925d9fefe89a0adc369a904b84336b6118fd270879adb5565eecce9633bb1e81965b6712ff930c92616a58e827b667f534c23a4d2fa0a72'
